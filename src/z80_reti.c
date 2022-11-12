8bd7c66c6020163afb317772206a7ae92e4b2a8e
Adrià Giménez Pastor <adriagipas@gmail.com>
2022-11-12 20:34:10 +0100

    Implementat z80_reti

---
+++ src/z80_reti.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2012-2022 Adrià Giménez Pastor.
+ *
+ * This file is part of adriagipas/GG.
+ *
+ * adriagipas/GG is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * adriagipas/GG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/*
+ *  z80_reti.c - Implementa Z80_reti_signal.
+ *
+ */
+
+
+void
+Z80_reti_signal (void)
+{
+} /* end Z80_reti_signal */
